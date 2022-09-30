cd1c6770970cb3319be94842e2e4426385f2a036
Banzyworld <Banzyworld@users.noreply.github.com>
2022-09-30 21:58:34 +0100

    rendered imported components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+import ResponsiveAppBar from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+// import Blog from './Components/Blog/Blog';
+// import Contact from './Components/Contact/ContactMe';
+// import {Routes, Route} from 'react-router-dom';
+import AnimatedCursor from "react-animated-cursor";
+
+import Projects from './Components/Projects/Projects.jsx';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+
+      <AnimatedCursor color="255,255,255" />
+      <ResponsiveAppBar />
+      <Home />
+      <About />
+
+      <Projects />
+
+
+
+
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element="" />
+        <Route path="Blog" element={<Blog />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes> */}
+      {/* <Route path='Projects' element={<Projects />} /> */}
+
+      {/* <Footer/> */}
     </div>
   );
 }
