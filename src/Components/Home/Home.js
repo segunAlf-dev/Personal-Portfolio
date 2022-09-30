cc7198f721116437de7c6509b3f4818be288bea9
Banzyworld <Banzyworld@users.noreply.github.com>
2022-09-30 08:01:42 +0100

    added intro section with my social links

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Home.css';
+
+
+const Home = () => {
+  
+  return (
+      <div className='container' >
+         <div className='Intro_Text'> <h1> Hello👋,<br /> <span>I'm</span> Segun <span> Alofun</span> - Curious <br/>  designer and developer <br />based in Lagos Nigeria.<br/>
+          </h1>
+       
+         </div>     
+       
+        {/* Social Links */}
+           <div className='social-links'>
+          <ul>
+            <li className=''>
+              <a className='' href="https://www.linkedin.com/in/banzyworld/" target={"_self"}>Linkedin </a>
+            </li>
+             <li className=''>
+              <a className='' href="https://github.com/Banzyworld" target={"_self"}>Github</a>
+            </li>
+              <li className=''>
+              <a className='' href="https://twitter.com/Banzyworld" target={"_self"}>Twitter</a>
+            </li>
+             <li className=''>
+              <a className='' href="mailto:segunalofun@protonmail.com" target={"_self"}>Email</a>
+            </li>
+                
+          </ul>
+        </div>
+
+    
+    </div>
+  )
+}
+
+export default Home
